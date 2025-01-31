6501200c89066e942f9224945d871224231b14f3
Opilane TTHK <opilane@tthk.ee>
2025-01-31 10:57:56 +0200

    Nevermind lmao

--- SPA_APP_2024-main/Backend/Model/Event.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Model;
-
-public record Event{
-    public int Id {get; set;}
-    public string? Type {get; set;}
-    public string? Description {get; set;}
-    public string? Location {get; set;}
-    public DateTime Date {get; set;}
-}
